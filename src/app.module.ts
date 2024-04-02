3763375c4232b11566a06c3afa9cd1b2699555d2
Matthias Brumant <matthias.brumant@gmail.com>
2024-04-02 19:04:18 +0200

    connection à la base de données réussie

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+// Charge les variables d'environnement à partir du fichier .env
+require("dotenv").config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [],
+      synchronize: true,
+    })]
+  ,
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
