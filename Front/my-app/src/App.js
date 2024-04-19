149af6aba5f546e5c376ad7636d0afdfef37409f
Matthias Brumant <matthias.brumant@gmail.com>
2024-04-19 14:39:34 +0200

    Formulaire d'inscription fonctionnel : connecté au back-end

--- Front/my-app/src/App.js
+++ Front/my-app/src/App.js
@@ -3,70 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <html>
-      <head>
-        <title>HTML Form</title>
-      </head>
-      <body>
-        <form>
-          <fieldset>
-            <legend>
-              User personal information
-            </legend>
-            <label
-            >Enter your full name</label
-            >
-            <input type="text" name="name" />
-            <label>Enter your email</label>
-            <input
-              type="email"
-              name="email"
-            />
-            <label>Enter your password</label>
-            <input
-              type="password"
-              name="pass"
-            />
-            <label
-            >Confirm your password</label
-            >
-            <input
-              type="password"
-              name="confirmPass"
-            />
-            <label>Enter your gender</label>
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-            />Male
-            <input
-              type="radio"
-              name="gender"
-              value="female"
-            />Female
-            <input
-              type="radio"
-              name="gender"
-              value="others"
-            />Others
-            <label
-            >Enter your Date of
-              Birth</label
-            >
-            <input type="date" name="dob" />
-            <label>Enter your Address:</label>
-            <textarea
-              name="address"
-            ></textarea>
-            <input
-              type="submit"
-              value="submit"
-            />
-          </fieldset>
-        </form>
-      </body>
-    </html>
+    <>
+      <html>
+        <head>
+          <title>HTML Form</title>
+        </head>
+        <body>
+          <form action="http://localhost:8080/users" method="POST">
+            <fieldset>
+              <legend>
+                Inscription
+              </legend>
+              <label
+              >Enter your full name</label
+              >
+              <input type="text" name="Pseudo" />
+              <label>Enter your email</label>
+              <input
+                type="email"
+                name="email"
+              />
+              <label>Enter your password</label>
+              <input
+                type="password"
+                name="password"
+              />
+
+              <label
+              >Enter your Date of
+                Birth</label
+              >
+              <input type="date" name="dob" />
+              <input
+                type="submit"
+                value="submit"
+              />
+            </fieldset>
+          </form>
+        </body>
+      </html>
+    </>
   );
 }
 
