89eebc5d086157d4e70b44c3e9788ee219ed9238
Matthias Brumant <matthias.brumant@gmail.com>
2024-05-22 17:41:26 +0200

    Page profil : possibilité de se déconnecter

--- Front/my-app/src/pages/Profil.js
+++ Front/my-app/src/pages/Profil.js
@@ -1,11 +1,79 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profil() {
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch('http://localhost:8080/users/modify-password', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({ password })
+            });
+            const data = await response.json();
+
+            if (response.ok) {
+                setMessage('Mot de passe changé !');
+            } else {
+                setMessage('Erreur : Mot de passe inchangé');
+            }
+        } catch (error) {
+            setMessage(error.message);
+        }
+    };
+
+    async function logout() {
+        try {
+            const token = localStorage.getItem("token")
+            const response = await fetch('http://localhost:8080/auth/logout', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+
+            if (response.ok) {
+                setMessage('Vous êtes bien déconnecté !')
+                setTimeout(() => {
+                    navigate('/accueil');
+                }, 1500);
+            }
+            else {
+                setMessage('Echec de la déconnexion')
+            }
+        }
+        catch (error) {
+            setMessage(error.message)
+        }
+    }
     return (
-        <> <div>Changer votre mot de passe :</div>
-            <form action="http://localhost:8080/users/modify-password">
-                <input type="text"></input>
+        <>
+            <div>Changer votre mot de passe :</div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="submit"
+                    value="Changer Mot de passe"
+                />
             </form>
-        </>)
+            {message && <p>{message}</p>}
+
+            <button onClick={logout}>Se déconnecter :</button>
+            {message && <p>{message}</p>}
+        </>
+    );
 }
